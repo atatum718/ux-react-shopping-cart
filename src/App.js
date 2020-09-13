04c9bf37c92133328247e56ff59029adfe28153a
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-09-13 19:19:38 -0400

    made check box clickable and show true or false

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
                 // Your code here!
                 // Use setState to update the flag in state for the checkbox
                 // Hint: use event.target.checked
-                this.setState()
+                this.setState({ isOnMailingList: event.target.checked })
               }
             />
             Sign me up for the mailing list!
